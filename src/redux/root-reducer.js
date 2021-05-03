4613f10edd3991d11738f86cc6638641b9621bd2
jasonn0118 <tlswoals2006@gmail.com>
2021-05-03 01:37:53 -0700

    Created cart-dropdown component and added cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import cartReducer from './cart/cart.reducer';
 
 import userReducer from './user/user.reducer';
 
 //Combine one giant object.
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
